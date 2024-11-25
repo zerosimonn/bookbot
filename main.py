79f3fdb104d6359bcdb14524d8028a7d5bd4a42b
zerosimonn <de.regt.simon@gmail.com>
2024-11-25 17:21:28 +0100

    Assignment 11 with boots

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def main():
     chars_dict = get_num_char(text)
 
     print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{number_of_words} words found in the document")
+    print(f"{number_of_words} words found in the document\n")
+    reporting(chars_dict)
+
     
-    result = reporting(chars_dict)
-    print(result)
     print("--- End report ---")
 
 def get_book_text(path):
@@ -30,16 +30,28 @@ def get_num_char(text):
             my_dict.update({letter : 1})
     return my_dict    
 
+
+def sort_on(VAR):
+    return VAR["num"]
+
 def reporting(my_dict):
-    sorted_items = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
-    sorted_dict = dict(sorted_items)
+    #sorted_items = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+    #sorted_dict = dict(sorted_items)
+    sorted_list = []
+    for char, count in my_dict.items():
+        if char.isalpha() and char != " ":
+            char_dict = {"char" : char, "num" : count}
+            sorted_list.append(char_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+#sort_on (correct) - gives Python the function to use
+#sort_on() (incorrect) - tries to run the function right away with no arguments
+#sort_on(VAR) (incorrect) - tries to run the function right away with VAR
+#call the function, not a value, so sort_on and not sort_on()
+            
+            #print(f"The '{char}' character was found {count} times")
+    for item in sorted_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-    for key, value in sorted_dict.items():
-        if key.isalpha() and key != " ":
-            print(f"The '{key}' character was found {value} times")
-        else:  
-            None
 
-    # return(sorted_dict)
 
 main()
